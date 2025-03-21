6cf8bcca9f5e3407e3863ad35339d1e83c51b8c8
mathew <mathew1prince@gmail.com>
2025-03-21 12:03:16 +0530

    bannercomponet

--- src/render-components/Banner/Banner.tsx
+++ src/render-components/Banner/Banner.tsx
@@ -1,5 +1,23 @@
 import './Banner.scss';
+import BannerComponent from '../../components/Banner/Banner';
+import mathew from '../../assets/mathew.jpg';
 
 export const Banner = () => {
-  return <div>Banner Component</div>;
+  return (
+    <>
+      
+      <BannerComponent
+      
+      
+      title="Welcome"
+      subtitle="Hey! I'm [MATHEW.A],a frontend developer who loves building websites and making them look great. I work with React, Angular, and JavaScript to create smooth and interactive user experiences.
+I’ve worked on projects like e-commerce sites, dashboards, and student management systems, handling everything from UI design to functionality."
+      backgroundImage={mathew}
+      buttonLabel="Contact Us"
+      onButtonClick={() => alert("Button Clicked!")}
+    />
+    
+
+    </>
+  )
 };
